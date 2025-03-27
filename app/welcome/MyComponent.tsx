a59976dbc927d176698933a64eb057836fd59f85
VHarsha8 <harshavardhan6304@gmail.com>
2025-03-27 20:09:09 +0530

    Update

--- app/welcome/MyComponent.tsx
+++ app/welcome/MyComponent.tsx
@@ -5,10 +5,10 @@ const MyComponent: React.FC = () => {
   return (
     <div className='flex flex-col  w-full border-red-600  m-auto  pb-6  h-auto justify-start items-start'>
      <h1 className='text-white/70 text-5xl selection:text-[#FFFF00] selection:bg-[#1111] font-semibold font-sans'> Developer <span className='text-red-500'>\</span> Design </h1> <br/>
-     <h2 className='text-white/85 text-sm selection:text-[#FFFF00] font-sans w-full md:w-4/6 selection:bg-[#1111] '>Hi! 👋 I'm Harsha, a creative technologist who loves crafting stunning digital experiences.
-With a passion for UI/UX and front-end magic, I turn ideas into reality.
-I thrive on innovation, exploring new tools, and solving exciting design challenges.
-Pushing tech boundaries every day, I create seamless, engaging, and unforgettable products!
+     <h2 className='text-white/85 text-sm selection:text-[#FFFF00] font-sans w-full md:w-4/6 selection:bg-[#1111] '>
+     Hi! 👋 I'm Harsha, a creative technologist passionate about developing websites and digital experiences.
+With expertise in UI/UX and front-end development, I turn ideas into reality.
+I love exploring new tools, solving design challenges, and pushing tech boundaries!
 
 
 
